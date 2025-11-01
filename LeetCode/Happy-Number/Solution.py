0be6c4132a1eadfb1ffd026f4fb5b936478356b5
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-11-01 23:23:36 +0530

    Add solution for Happy Number

---
+++ LeetCode/Happy-Number/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        count=0
+        for i in range(200):
+            a=0
+            while n>0:
+                a=a+(n%10)**2
+                n=n//10
+                count+=1
+            if a==1:
+                return True
+            elif count<100:
+                n=a
+                continue
+            else:
+                return False
