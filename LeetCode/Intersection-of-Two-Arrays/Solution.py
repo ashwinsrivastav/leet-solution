42c753e7a5c37a22f8f9adda3f657610287e092b
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-11-01 23:28:32 +0530

    Add solution for Intersection of Two Arrays

---
+++ LeetCode/Intersection-of-Two-Arrays/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums3=[]
+        for i in nums1:
+            if i in nums2:
+                if i not in nums3:
+                    nums3.append(i)
+        return nums3
