3459b847db6e91ac3bd1922bd778245a2bd9a16a
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-11-04 00:08:28 +0530

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.py
+++ LeetCode/Single-Number/Solution.py
@@ -1,7 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums=sorted(nums)
-        for i in nums:
-            nums.remove(i)
-            if i not in nums:
-                return i
+        freq = {}
+        #{"1" :2 ,0 :1 :} -> 
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1  # same as Java getOrDefault(num, 0) + 1
+
+        # filter the one that appears only once
+        for key, value in freq.items():
+            if value == 1:
+                return key
