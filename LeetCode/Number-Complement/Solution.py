ae3515084e3bd1942b4ed2c2aa8a612064ff9476
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-11-13 23:24:50 +0530

    Add findComplement method in Solution class
    Implement findComplement method to calculate the bitwise complement of a number.

--- LeetCode/Number-Complement/Solution.py
+++ LeetCode/Number-Complement/Solution.py
@@ -1,8 +1,9 @@
-69
-911
-1
-1073741824
-2100003647
-2147483647
-1073741823
-341
+class Solution:
+    def findComplement(self, num: int) -> int:
+        num=bin(num)[2:];b=''
+        for i in num:
+            if i=='0':
+                b=b+'1'
+            else:
+                b=b+'0'
+        return int(b,2)
