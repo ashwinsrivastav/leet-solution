9af949db5cf723351f2d9f2c42d671faca7b8bc1
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-11-19 17:44:17 +0530

    Update Solution.py

--- LeetCode/1-bit-and-2-bit-Characters/Solution.py
+++ LeetCode/1-bit-and-2-bit-Characters/Solution.py
@@ -1,1 +1,15 @@
-return random.choice([True, False])
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        try:
+            while len(bits)>1:
+                if bits[0]==1:
+                    bits.pop(0)
+                    bits.pop(0)
+                else:
+                    bits.pop(0)
+            if bits[0]==0:
+                return True
+            else: return False
+        except:
+            return False
+        return True
