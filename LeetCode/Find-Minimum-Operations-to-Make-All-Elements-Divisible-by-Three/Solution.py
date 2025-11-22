a59de528d47b6429c30ff8a8d76768c57c095507
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-11-22 21:36:42 +0530

    Refactor minimumOperations to Python class method

--- LeetCode/Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three/Solution.py
+++ LeetCode/Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three/Solution.py
@@ -1,3 +1,13 @@
-function minimumOperations(nums: number[]): number {
-   return nums.filter((num)=> num % 3 !== 0).length
-}; 
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        count=0
+        for i in range(len(nums)):
+            if nums[i]%3==0:
+                continue
+            a=nums[i]%3
+            if a==1:
+                nums[i]-=1
+            else:
+                nums[i]+=1
+            count+=1
+        return count
