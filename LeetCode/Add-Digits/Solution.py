090118208f6c5250f58cd43de0192cddf994b7fd
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-12-02 21:53:14 +0530

    Add solution for Add Digits

--- LeetCode/Add-Digits/Solution.py
+++ LeetCode/Add-Digits/Solution.py
@@ -1,3 +1,12 @@
-def add_digits(num)
-    num==0 ? 0:(num%9==0 ? 9 : num%9)
-end
+1class Solution:
+2    def addDigits(self, num: int) -> int:
+3        sum=0
+4        while num%10!=num:
+5            while num>0:
+6                sum=sum+num%10
+7                num=int(num/10)
+8            num=sum
+9            sum=0
+10        return num
+11        
+12
