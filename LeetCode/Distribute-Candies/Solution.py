0ca87b0543844f5ecf8f3a372e5104a586bfde89
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-12-23 22:48:02 +0530

    Add solution for Distribute Candies

---
+++ LeetCode/Distribute-Candies/Solution.py
@@ -0,0 +1,10 @@
+	<B style="font-size: 25px">LOGIN<BR>
+	<BR></B><br> <a href="adminLogin.jsp"><button
+			style="margin-left: 20px" type="button">
+			<B>ADMIN LOGIN</B>
+		</button></a> <a href="UnderwriterLogin.jsp"><button style="margin-left: 20px"
+			type="button">
+			<B>UNDERWRITER</B>
+		</button></a>
+</DIV>
+<div id="footer">&#169; QVY INSURANCE ONLINE 2024</div>
