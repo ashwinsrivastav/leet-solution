c7307c5f57c0f308b477162388893008d67fff45
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2025-12-26 22:11:46 +0530

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.py
+++ LeetCode/Palindrome-Number/Solution.py
@@ -1,4 +1,7 @@
 1class Solution:
 2    def isPalindrome(self, x: int) -> bool:
 3        x=str(x)
-4        return (x[::-1])==x
+4        if (x[-1::-1])==x:
+5            return True
+6        else:
+7            return False 
