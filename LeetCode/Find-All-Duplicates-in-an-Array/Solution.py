1170bf3b126c16834ffcff429f2c13dbd508c656
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-02 22:21:22 +0530

    Add solution for Find All Duplicates in an Array

---
+++ LeetCode/Find-All-Duplicates-in-an-Array/Solution.py
@@ -0,0 +1,18 @@
+     vector<int>ans;
+
+    for(int i=0;i<nums.size();i++)
+    {
+        int temp = abs(nums[i]);
+
+        if(nums[temp-1] < 0){
+            ans.push_back(temp);
+        }else
+        {
+            nums[temp-1] *=-1;
+        }
+    }
+     return ans;
+ }
+
+    
+  
