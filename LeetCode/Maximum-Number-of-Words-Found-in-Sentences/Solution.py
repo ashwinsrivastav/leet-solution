4492ee25838090809a271267e569d1316bad2a8c
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-05 15:09:52 +0530

    Update Solution.py

--- LeetCode/Maximum-Number-of-Words-Found-in-Sentences/Solution.py
+++ LeetCode/Maximum-Number-of-Words-Found-in-Sentences/Solution.py
@@ -1,1 +1,8 @@
-let word = sentences.split(' ')
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        space=0;max=0
+        for i in sentences:
+            space=i.count(" ")
+            if space>max:
+                max=space
+        return max+1
