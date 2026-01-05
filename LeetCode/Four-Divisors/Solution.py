f555fca2c30f29b0d0cee6642e02c47dafafdfdc
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-05 15:12:44 +0530

    Implement function to sum four divisors from list

--- LeetCode/Four-Divisors/Solution.py
+++ LeetCode/Four-Divisors/Solution.py
@@ -1,8 +1,19 @@
-[16,21,4,7]
-[21,21]
-[1,2,3,4,5]
-[6,8,10,14]
-[15,16,17,18]
-[33,22,26,27,28]
-[6,10,14,15,21,22,26,33,35,38,39,46,51,55,57,58,62,65,69,74]
-[6,10,14,15,21,22,26,33,35,38,39,46,51,55,57,58,62,65,69,74,77,82,85,86,87,91,93,94,95,106,111,115,118,119,122,129,133,134,141,142]
+def fact(n)-> list:
+    factt=[]
+    for i in range(1,int(sqrt(n))+1):
+        if n%i==0:
+            factt.append(i)
+            if n//i != i:
+                factt.append(n//i)
+    return factt 
+
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        factors=[];summ=0
+        for i in nums:
+            factors.extend(fact(i))
+            #factors.append(i)
+            if len(factors)==4:
+                summ+=sum(factors)
+            factors.clear()
+        return summ
