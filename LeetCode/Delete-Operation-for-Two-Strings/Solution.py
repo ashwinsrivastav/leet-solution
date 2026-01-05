dc63c460b46f63b1632f65fd6ecba8e104719190
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-05 15:06:04 +0530

    Add solution for Delete Operation for Two Strings

--- LeetCode/Delete-Operation-for-Two-Strings/Solution.py
+++ LeetCode/Delete-Operation-for-Two-Strings/Solution.py
@@ -1,16 +1,1 @@
-        ///insert case
-        int ans1=1+helper(s1,s2,i+1,j);
-        //delete
-        int ans2=1+helper(s1,s2,i,j+1);
-        ans=Math.min(ans1,ans2);
-    }
-    return dp[i][j]=ans;
-}
-public int minDistance(String str1, String str2) {
-    dp=new int[str1.length()+1][str2.length()+1];
-    for(int []arr:dp){
-        Arrays.fill(arr,-1);
-    }
-    return helper(str1,str2,0,0);
-    
-}
+return (word1.size()+word2.size())-2*lcs(word1,word2))
