c00498178e954e918ba904a9e13adf1bfabec2af
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-06 20:46:33 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.py
+++ LeetCode/Two-Sum/Solution.py
@@ -1,12 +1,13 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
-    }
-  }
- }
+1from numpy import *
+2class Solution:
+3    def twoSum(self, nums: List[int], target: int) -> List[int]:
+4        for i in range(len(nums)):
+5            for j in range(len(nums)):
+6                if nums[i]+nums[j]==target:
+7                    if i!=j:
+8                     break
+9            if j!=len(nums)-1:
+10                break
+11        return [j,i]
+12
+13        
