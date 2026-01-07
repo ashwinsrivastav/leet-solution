a9566df31e1e619605f1f09616af0caf6b190d96
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-07 23:32:27 +0530

    Implement date reformatting in Python
    Refactor date reformatting logic and replace C++ map with Python dictionary.

--- LeetCode/Reformat-Date/Solution.py
+++ LeetCode/Reformat-Date/Solution.py
@@ -1,4 +1,11 @@
-std::unordered_map<std::string, std::string> monthIndex = { 
-    {"Jan", "01"}, {"Feb", "02"}, {"Mar", "03"}, {"Apr", "04"}, {"May", "05"}, {"Jun", "06"},
-    {"Jul", "07"}, {"Aug", "08"}, {"Sep", "09"}, {"Oct", "10"}, {"Nov", "11"}, {"Dec", "12"} 
-};
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        date=date.split();rev=''
+        months = {"Jan": "01","Feb": "02","Mar": "03","Apr": "04","May": "05","Jun": "06","Jul": "07","Aug": "08","Sep": "09","Oct": "10","Nov": "11","Dec": "12"}
+        rev+=date[2]+"-"
+        rev+=months[date[1]]+"-"
+        if len(date[0])==3:
+            rev+="0"+date[0][0]
+        else:
+            rev+=date[0][:2]
+        return rev
