4bb04c6bf46e96bdd86ccc5abbafbefb42a0d2b1
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-27 13:58:18 +0530

    Refactor kthSmallest method for simplicity

--- LeetCode/Kth-Smallest-Element-in-a-Sorted-Matrix/Solution.py
+++ LeetCode/Kth-Smallest-Element-in-a-Sorted-Matrix/Solution.py
@@ -1,29 +1,5 @@
-    m,n = len(matrix), len(matrix[0])
-
-    def get_elements(val):
-        elements = 0
-        i,j = m-1,0
-
-        while i>=0 and j<n:
-            if matrix[i][j] > val:
-                i-=1
-            else:
-                elements+=i+1
-                j+=1
-        
-        return elements
-
-    l = matrix[0][0]
-    r = matrix[-1][-1]
-
-    while l<r:
-        mid = (l+r) // 2
-        
-        elements = get_elements(mid)
-        
-        if elements < k:
-            l = mid + 1
-        elif elements >= k:
-            r = mid
-
-    return l
+ class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        lis=sum(matrix,[])
+        lis.sort()
+        return lis[k-1]
