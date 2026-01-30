1dd1a3c5f79f891a2603d084448a360070f4aed3
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-01-30 11:41:55 +0530

    Add solution for Count Items Matching a Rule

--- LeetCode/Count-Items-Matching-a-Rule/Solution.py
+++ LeetCode/Count-Items-Matching-a-Rule/Solution.py
@@ -1,13 +1,13 @@
-public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-    int count=0;
-
-
-for(List list:items){
-    if(list.contains(ruleValue)){
-        count++;
-        
-    }
-}return count;
-
-    
-}
+1class Solution:
+2    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+3        matchh=0
+4        for i in items:
+5            if ruleKey=="color" and  ruleValue==i[1]:
+6                matchh+=1
+7            elif ruleKey == "type" and ruleValue==i[0]:
+8                matchh+=1
+9            elif ruleKey == "name" and ruleValue==i[2]:
+10                matchh+=1
+11        return matchh
+12
+13
