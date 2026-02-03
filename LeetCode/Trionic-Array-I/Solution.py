bd3275d5d3d2c6166e7b945e8c153e08aae9ea34
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-03 18:31:39 +0530

    Add isTrionic method to Solution class
    Implement isTrionic method to check if the array follows a trionic pattern.

--- LeetCode/Trionic-Array-I/Solution.py
+++ LeetCode/Trionic-Array-I/Solution.py
@@ -1,4 +1,38 @@
-[8,9,4,6,1]
-[1,3,5,4,2,6]
-[5,9,1,7]
-[3,7,1]
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        count=0;index=0;lim=len(nums)-2
+        while nums[index]<nums[index+1]:
+            count=1
+            if index<lim:
+                index+=1
+            else:
+                return False
+        if count==1:
+            if nums[index]>nums[index+1]:
+                while nums[index]>nums[index+1]:
+                    count=2
+                    if index<lim:
+                        index+=1
+                    else:
+                        return False
+            else:
+                return False
+        else:
+            return False
+        if count==2:
+            if nums[index]<nums[index+1]:
+                while nums[index]<nums[index+1]:
+                    count=3
+                    if index<lim:
+                        index+=1
+                    else:
+                        break
+                if nums[index]<nums[index+1]:
+                    pass
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+        return True
