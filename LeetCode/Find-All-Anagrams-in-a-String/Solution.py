10ec7c442307e9b0237534f9c36e011ac2dfef3a
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-04 15:09:53 +0530

    Optimize findAnagrams using Counter for anagram check
    Refactor findAnagrams method to use Counter for efficiency.

--- LeetCode/Find-All-Anagrams-in-a-String/Solution.py
+++ LeetCode/Find-All-Anagrams-in-a-String/Solution.py
@@ -1,10 +1,14 @@
-    len_string_test =len(p)
-    list_start = []
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        pp=Counter(p);res=[]
+        length=len(p)
+        for i in range(len(s)-length+1):
+            temp=s[i:i+length]
+            slider=Counter(temp)
+            if pp==slider:
+                res.append(i)
+        return res
+            
 
-    for i in range(len(s)):
-        list_test =s[i:i+len_string_test]
-        if isAnag(list_test,p):
-            list_start.append(i)
-
-    return list_start
     
