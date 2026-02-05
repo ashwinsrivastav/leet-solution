5f8544b5f1168bdc00386ca069cbb15da2eb3d47
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-05 23:20:55 +0530

    Add solution for Transformed Array

--- LeetCode/Transformed-Array/Solution.py
+++ LeetCode/Transformed-Array/Solution.py
@@ -1,21 +1,8 @@
-def nextindex(n,lenght):
-    if n>=0:
-        return n%lenght
-    else:
-        return n+lenght
-class Solution:
-    def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        lenght=len(nums);res=[]
-        for i in range(lenght):
-            n=nums[i]
-            if n!=0:
-                newindex=nextindex(i+n,lenght)
-                while abs(newindex)>lenght:
-                    newindex=nextindex(newindex,lenght)
-                res.append(nums[newindex])
-            else:
-                res.append(nums[i])
-        return res
-
-    
-    
+[0]
+[-100]
+[100]
+[-100,-100]
+[100,100]
+[-100,100]
+[100,-100]
+[0,0]
