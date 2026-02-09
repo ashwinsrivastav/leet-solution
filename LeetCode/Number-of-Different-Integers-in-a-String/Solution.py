cd8b3f71d54a23d0c59123cf2d294d811fd05c9a
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-09 18:34:07 +0530

    Add solution for Number of Different Integers in a String

--- LeetCode/Number-of-Different-Integers-in-a-String/Solution.py
+++ LeetCode/Number-of-Different-Integers-in-a-String/Solution.py
@@ -1,17 +1,8 @@
-1class Solution:
-2    def numDifferentIntegers(self, word: str) -> int:
-3        nums='0123456789';a='';num=[]
-4        for i in word:
-5            if i in nums:
-6                a+=i
-7                continue
-8            if a!='':
-9                num.append(int(a))
-10            a=''
-11        if a!='':
-12            num.append(int(a))
-13        return len(set(num))
-14
-15
-16
-17
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        matches = re.findall(r'-?\d*\.?\d+', word)
+        ans=[int(i) for i in matches]
+        return len(set(ans))
+
+
+
