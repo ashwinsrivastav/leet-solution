a6cd34a636c5de0b12d051d894fbbad49b0cd302
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-10 12:55:47 +0530

    Add solution for Word Pattern

--- LeetCode/Word-Pattern/Solution.py
+++ LeetCode/Word-Pattern/Solution.py
@@ -1,15 +1,15 @@
-def check(dict,a,s,pattern):
-    if len(set(a))!=len(set(s)) or len(pattern)!=len(s):
-        return False
-    for val,i in zip(a,range(len(s))):
-        if dict[val]!=s[i]:
-            return False
-    else:
-        return True
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        dict={};a=[];s=s.split()
-        for i,j in zip(pattern,s):
-            a.append(i)
-            dict[i]=j
-        return check(dict,a,s,pattern)
+1def check(dict,a,s,pattern):
+2    if len(set(a))!=len(set(s)) or len(pattern)!=len(s):
+3        return False
+4    for val,i in zip(a,range(len(s))):
+5        if dict[val]!=s[i]:
+6            return False
+7    else:
+8        return True
+9class Solution:
+10    def wordPattern(self, pattern: str, s: str) -> bool:
+11        dict={};a=[];s=s.split()
+12        for i,j in zip(pattern,s):
+13            a.append(i)
+14            dict[i]=j
+15        return check(dict,a,s,pattern)
