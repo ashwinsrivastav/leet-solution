b4414d42439034b4b9bb66e75cba780420e5e052
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-10 14:21:07 +0530

    Add hasMatch method for pattern matching
    Implemented a method to check if a string matches a given pattern with wildcards.

--- LeetCode/Substring-Matching-Pattern/Solution.py
+++ LeetCode/Substring-Matching-Pattern/Solution.py
@@ -1,10 +1,10 @@
-"wwmwww"
-"wm*"
-"tokk"
-"t*t"
-"mqmmmqmqbmqq"
-"mmqm*b"
-"mqmmqcmmqdmmqemmmmmmqmqbmqq"
-"mmqm*b"
-"kakkkkaa"
-"*aka"
+class Solution:
+    def hasMatch(self, s: str, p: str) -> bool:
+        p=list(filter(None,p.split("*")));k=s
+        for i in p:
+            if i in k:
+                cut=k.index(i)
+                k=k[cut+len(i):]
+            else:
+                return False
+        return True
