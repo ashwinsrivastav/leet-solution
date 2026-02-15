1d37b64ce6fb762a8a6a1fc23bf8bb9cfbba5299
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-15 20:14:32 +0530

    Add solution for Add Binary

--- LeetCode/Add-Binary/Solution.py
+++ LeetCode/Add-Binary/Solution.py
@@ -1,3 +1,3 @@
-class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        return (str(bin(int(a,2)+int(b,2)))).removeprefix("0b")
+1class Solution:
+2    def addBinary(self, a: str, b: str) -> str:
+3        return (str(bin(int(a,2)+int(b,2)))).removeprefix("0b")
