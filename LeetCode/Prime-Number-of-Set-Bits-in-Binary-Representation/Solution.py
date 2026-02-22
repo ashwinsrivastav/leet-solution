2b7fd4752dbe735935a5376ab2e30e39af645761
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-22 12:06:15 +0530

    Add method to count prime set bits in range
    Implement countPrimeSetBits method to count numbers with prime set bits in binary representation.

--- LeetCode/Prime-Number-of-Set-Bits-in-Binary-Representation/Solution.py
+++ LeetCode/Prime-Number-of-Set-Bits-in-Binary-Representation/Solution.py
@@ -1,16 +1,15 @@
-1
-10001
-123
-10123
-500000
-510000
-999000
-1000000
-262144
-272144
-700000
-709999
-42
-10042
-888888
-898888
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        def prime(n):
+            if n<3:
+               return True if n==2 else False
+            for i in range(2,n):
+                if n%i==0:
+                    return False
+            return True
+        count=0
+        for i in range(left,right+1):
+            n=bin(i).count("1")
+            if prime(n):
+                count+=1
+        return count
