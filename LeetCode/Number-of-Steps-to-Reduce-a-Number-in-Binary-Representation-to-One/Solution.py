04421499d01d2827661c293a22b9edd517b6e2e2
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-02-26 12:55:36 +0530

    Refactor NumSteps method to Python implementation

--- LeetCode/Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One/Solution.py
+++ LeetCode/Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One/Solution.py
@@ -1,11 +1,11 @@
-public int NumSteps(string s) {
-    int zeroCount = 1, oneCount = 0, n = s.Length;
-
-    for(int i = n - 1; i >= 0; i--)
-    {
-        if(s[i] == '1') oneCount++;
-        if(oneCount > 0 && s[i] == '0') zeroCount++;
-    }
-
-    return oneCount == 1 ? n - 1 : zeroCount + n;
-}
+class Solution:
+    def numSteps(self, s: str) -> int:
+        p=int(s,2);steps=0
+        while p!=1:
+            if p%2==0:
+                p=p//2
+            else:
+                p+=1
+            steps+=1
+        return steps
+            
