3c9067bedb76b469d5813fb2dad655bd9bebf1d0
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-03-06 21:56:04 +0530

    Implement maxFreqSum method to calculate frequency sum
    Added a class Solution with a method to calculate the sum of the most frequent vowel and consonant counts in a given string.

--- LeetCode/Find-Most-Frequent-Vowel-and-Consonant/Solution.py
+++ LeetCode/Find-Most-Frequent-Vowel-and-Consonant/Solution.py
@@ -1,8 +1,10 @@
-"tnkdlyyhzhpfdhlatnnuxetutiuxf"
-"mtiowoyaevknjyyvacnpgjmrgxxqzejtxqrx"
-"syljhvubasvunsuznsvjwaktioqzxcqtwhgcqqedgfxhgakvej"
-"mufjkpdirrjevyuewfcsbfrxuctajfmkjwkbfxykgrrajhmwcicnzxkkclhhiernycnscxzc"
-"drlbbncfojopkivbvsayyuclvhxxmutltwmbzrmnqjeazjkbguhmfo"
-"ichuixebthgosckfnoglptsftiuhgbtmahmzgxbukxxprdazmcmtheumrqgpdwakufcqknvjjyjxymrxhbwlsvram"
-"zejizlzuacvxvbrbfneldynfcpovhtkrjvcangtkmiqfodgubuauwinpvhzofgmmzimrtrzlfeidmpfaxlyjtcbhphruse"
-"cuwzpfodirf" 
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        vowel=('a', 'e', 'i', 'o','u')
+        Vfeq={1:0};Cfeq={1:0}
+        for i in s:
+            if i in vowel:
+                Vfeq[i]=Vfeq.get(i,0)+1
+            else:
+                Cfeq[i]=Cfeq.get(i,0)+1
+        return max(Vfeq.values()) + max(Cfeq.values())
