3483f15e9d25fcf0fc60c2c0af7dd381ffc3dfdd
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-03-09 21:44:39 +0530

    Refactor trimTrailingVowels method to use rstrip

--- LeetCode/Trim-Trailing-Vowels/Solution.py
+++ LeetCode/Trim-Trailing-Vowels/Solution.py
@@ -1,16 +1,9 @@
 class Solution:
     def trimTrailingVowels(self, s: str) -> str:
-        result = []
-        vowels = ['a','e','i','o','u']
-        result = list(s)
-        n=len(result)
-        last_pointer=0
-        #temp = 
-        for i in range(len(result)):
-            if result[n -1 - i] not in vowels:
-                return s[0:n - i]
-
-        return ''
-
-       
-        
+        vowel={'a', 'e', 'i', 'o', 'u'};d=""
+        for i in s[::-1]:
+            if i in vowel:
+                d=i+d
+            else:
+                break
+        return s.rstrip(d)
