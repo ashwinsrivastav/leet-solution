ffed6144686250773127150f5b591df6073222ca
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-03-12 22:15:25 +0530

    Add backspaceCompare method to Solution class
    Implement backspace string comparison using stacks.

--- LeetCode/Backspace-String-Compare/Solution.py
+++ LeetCode/Backspace-String-Compare/Solution.py
@@ -1,2 +1,14 @@
-"lcdfawmr#rnznsckcgautpinbqgkqjewc"
-"bgglnwv#qaju#cileyddvulnwmchzpwzb#################################lcdfawmr#rnznsckcgautpinbqgkqjewc"
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1="";stack2=""
+        for i in s:
+            if i!="#":
+                stack1+=i
+            else:
+                stack1=stack1[:-1]
+        for i in t:
+            if i!="#":
+                stack2+=i
+            else:
+                stack2=stack2[:-1]
+        return stack1==stack2
