7f3d1af48017b7c6acf4e2f7d1e589c52c18c4f2
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-03-20 23:09:20 +0530

    Add solution for Move Zeroes

--- LeetCode/Move-Zeroes/Solution.py
+++ LeetCode/Move-Zeroes/Solution.py
@@ -1,13 +1,12 @@
-class Solution:
-    def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
-        """
-        pos = 0
-        
-        for i in range(len(nums)):
-            el = nums[i]
-            if el != 0:
-                nums[pos], nums[i] = nums[i], nums[pos]
-                pos += 1
+1class Solution:
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        count=0
+7        for i in sorted(nums, reverse=True):
+8            if i==0:
+9                nums.remove(i)
+10                count+=1
+11        for i in range(count):
+12            nums.append(0)
