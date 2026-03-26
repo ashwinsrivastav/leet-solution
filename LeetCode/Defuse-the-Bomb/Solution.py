fc746031321bbddb3ae054238d48bcd13af9801a
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-03-26 23:16:16 +0530

    Refactor decrypt method for better readability

--- LeetCode/Defuse-the-Bomb/Solution.py
+++ LeetCode/Defuse-the-Bomb/Solution.py
@@ -1,19 +1,20 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        res=[];length=len(code)
-        if k>0:
+        res = []
+        length = len(code)
+        if k > 0:
             for i in range(length):
-                sum=0
-                for l in range(i+1,k+i+1):
-                    sum+=code[l%length]
+                sum = 0
+                for l in range(i + 1, k + i + 1):
+                    sum += code[l % length]
                 res.append(sum)
             return res
-        elif k<0:
+        elif k < 0:
             for i in range(length):
-                sum=0
-                for l in range(i-1,i+k-1,-1):
-                    sum+=code[l%length]
+                sum = 0
+                for l in range(i - 1, i + k - 1, -1):
+                    sum += code[l % length]
                 res.append(sum)
             return res
         else:
-            return [0]*len(code)
+            return [0] * len(code)
