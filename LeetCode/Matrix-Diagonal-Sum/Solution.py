5867219b3098a0276d20c1bcedbfbf8211e18a5b
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-03-31 00:02:22 +0530

    Add diagonalSum method to calculate matrix diagonal sum
    Implement diagonal sum calculation for a matrix.

--- LeetCode/Matrix-Diagonal-Sum/Solution.py
+++ LeetCode/Matrix-Diagonal-Sum/Solution.py
@@ -1,5 +1,13 @@
-            }
-        }
-    }
-    return sum;
-    
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        if len(mat[0])>1:
+            sum=0;lenght=len(mat[0])
+            for i in range(lenght):
+                sum+=mat[i][i]
+            for i,j in zip(range(lenght),range(lenght-1,-1,-1)):
+                sum+=mat[i][j]
+            if lenght%2!=0:
+                n=(lenght//2)
+                sum-=mat[n][n]
+            return sum
+        return mat[0][0]
