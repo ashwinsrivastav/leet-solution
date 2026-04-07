e8075fa54cfc16170031b8c569b6230496a1bd3b
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-07 22:41:26 +0530

    Convert C++ Morse code solution to Python
    Refactored the Morse code transformation logic from C++ to Python, using a dictionary for character mapping and a set to track unique transformations.

--- LeetCode/Unique-Morse-Code-Words/Solution.py
+++ LeetCode/Unique-Morse-Code-Words/Solution.py
@@ -1,10 +1,11 @@
-    for(const string& c : words)
-    { string temp;
-        for(char ch : c)
-        {
-            temp=temp+MorseCode[ch-'a'];
-        }
-        sets.insert(temp);
-    }
-    return sets.size();
-}
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        dic = {'a':".-", 'b':"-...", 'c':"-.-.", 'd':"-..", 'e':".", 'f':"..-.", 'g':"--.", 'h':"....", 'i':"..", 'j':".---", 'k':"-.-", 'l':".-..", 'm':"--", 'n':"-.", 'o':"---", 'p':".--.", 'q':"--.-", 'r':".-.", 's':"...", 't':"-", 'u':"..-", 'v':"...-", 'w':".--", 'x':"-..-", 'y':"-.--", 'z':"--.."}
+        sett=set();word=""
+        for i in words:
+            for j in i:
+                word+=dic[j]
+            if word not in sett:
+                sett.add(word)
+            word=""
+        return len(sett)
