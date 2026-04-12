520c2c50557077638068746b41976d6dbf7b61f6
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-12 22:39:39 +0530

    Implement decodeMessage method in Solution class

--- LeetCode/Decode-the-Message/Solution.py
+++ LeetCode/Decode-the-Message/Solution.py
@@ -1,7 +1,16 @@
-        for i in range(97,123):
-            print(f'"{chr(i)}",',end='')
-        
-        alpha=[]
-        for i in range(97,123):
-            alpha.append(chr(i))
-        print(alpha)
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        alpha=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        decoder={" ":" "}
+        key="".join((key.split(" ")));visited="";c=0
+        for i in key:
+            if i not in visited:
+                decoder[i]=alpha[c]
+                c+=1
+                visited+=i
+            if c>26:
+                break
+        res=""
+        for i in message:
+            res+=decoder[i] 
+        return res
