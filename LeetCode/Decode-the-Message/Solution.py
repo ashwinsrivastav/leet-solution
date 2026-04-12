bdafcf2d46f258f1b080d5f15e8c86cac16d2862
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-12 22:36:22 +0530

    Add solution for Decode the Message

---
+++ LeetCode/Decode-the-Message/Solution.py
@@ -0,0 +1,7 @@
+        for i in range(97,123):
+            print(f'"{chr(i)}",',end='')
+        
+        alpha=[]
+        for i in range(97,123):
+            alpha.append(chr(i))
+        print(alpha)
