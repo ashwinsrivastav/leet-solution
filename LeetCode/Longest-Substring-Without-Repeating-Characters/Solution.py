1f5861e47ea57b73798cd7ef19b1de5a214725a0
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-19 00:39:40 +0530

    Add solution for longest substring without repeating characters
    Implement lengthOfLongestSubstring method to calculate the length of the longest substring without repeating characters.

--- LeetCode/Longest-Substring-Without-Repeating-Characters/Solution.py
+++ LeetCode/Longest-Substring-Without-Repeating-Characters/Solution.py
@@ -1,1 +1,26 @@
-"bca"
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        count=0;uni=set();max=0;i=0
+        while i<len(s):
+            if s[i] not in uni:
+                uni.add(s[i])
+                count+=1
+                i+=1
+            else:
+                index=s.index(s[i])
+                s=s[index+1::]
+                i=0
+                if count>max:
+                    max=count
+                    uni.clear()
+                    uni.add(s[i])
+                    count=1
+                    i+=1
+                else:
+                    uni.clear()
+                    uni.add(s[i])
+                    count=1
+                    i+=1
+        if count>max:
+            max=count
+        return max
