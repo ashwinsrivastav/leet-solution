e54b3ba5dcdb69025a7345ca3a5571bfb1f4c6db
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-21 23:13:34 +0530

    Add removeDuplicates method for handling duplicates
    Implemented a method to remove duplicates from a sorted array while allowing at most two occurrences of each element.

--- LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.py
+++ LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.py
@@ -1,3 +1,11 @@
-for (int i = 0; i < k; i++) {
-    assert nums[i] == expectedNums[i];
-}
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        freq={};res=[]
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        for i in freq.keys():
+            if freq[i]>2:
+                res.extend([i]*2)
+            else:
+                res.extend([i]*freq[i])
+        nums[:]=res
