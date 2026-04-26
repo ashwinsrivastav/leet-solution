b24378ace6a6732ac62eea7d12f20126e4705e23
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-26 19:50:22 +0530

    Refactor partitionLabels method formatting

--- LeetCode/Partition-Labels/Solution.py
+++ LeetCode/Partition-Labels/Solution.py
@@ -1,20 +1,20 @@
-1class Solution:
-2    def partitionLabels(self, s: str) -> List[int]:
-3        freq={}
-4        for i in range(len(s)):
-5            freq[s[i]]=i
-6        i=0;res=[];max=0;last=-1
-7        while i<len(s):
-8            max=freq[s[i]]
-9            while i<=max:
-10                if max>=freq[s[i]]:
-11                    i+=1
-12                else:
-13                    max=freq[s[i]]
-14                    i+=1
-15            res.append(max-last)
-16            last=max
-17        return res
-18            
-19        
-20        
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        freq = {}
+        for i in range(len(s)):
+            freq[s[i]] = i
+        i = 0
+        res = []
+        max = 0
+        last = -1
+        while i < len(s):
+            max = freq[s[i]]
+            while i <= max:
+                if max >= freq[s[i]]:
+                    i += 1
+                else:
+                    max = freq[s[i]]
+                    i += 1
+            res.append(max - last)
+            last = max
+        return res
