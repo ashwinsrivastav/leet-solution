55f5322526485ffe3fe821907dd1a1fbb76967d4
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-04-26 19:46:54 +0530

    Implement furthestDistanceFromOrigin method
    Refactored the solution to calculate the furthest distance from origin based on moves.

--- LeetCode/Furthest-Point-From-Origin/Solution.py
+++ LeetCode/Furthest-Point-From-Origin/Solution.py
@@ -1,8 +1,8 @@
-"L_RL__R"
-"_R__LL_"
-"_______"
-"_RLR_LLR_R_LRLL_LR_RRL__LR__R_______L__R_RR__RL__L"
-"LL__L_R_LRRLRR_L_RRRRRL___RLRL_LRLLL_LLLRL__RL_RRR"
-"LLLLL"
-"RRRRR"
-"RRRR________"
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        countl=moves.count("L")
+        countr=moves.count("R")
+        count_=moves.count("_")
+        if countl>countr:
+            return countl+count_-countr
+        return countr+count_-countl
