0c1bf799814f30a9370ce340b36928ba51765af1
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-05-01 21:45:13 +0530

    Implement smallestRangeI method in Solution class

--- LeetCode/Smallest-Range-I/Solution.py
+++ LeetCode/Smallest-Range-I/Solution.py
@@ -1,2 +1,8 @@
-    return result;
-}
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        if abs(max(nums)-min(nums))<=k:
+            return 0
+        elif (max(nums)-k)-(min(nums)+k)<=0:
+            return 0
+        else:
+            return (max(nums)-k)-(min(nums)+k)
