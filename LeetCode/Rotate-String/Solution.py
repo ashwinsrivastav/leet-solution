fb96287af27abe3fc665e9be37811bd991460056
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-05-03 20:27:10 +0530

    Add solution for Rotate String

--- LeetCode/Rotate-String/Solution.py
+++ LeetCode/Rotate-String/Solution.py
@@ -1,2 +1,10 @@
-    def rotateString(self, s: str, goal: str) -> bool:
-        return len(s) == len(goal) and s in goal + goal
+1class Solution:
+2    def rotateString(self, s: str, goal: str) -> bool:
+3        a=s
+4        for i in s:
+5            a=a.removeprefix(i)
+6            a=a+i
+7            print(a)
+8            if a==goal:
+9                return True
+10        return False
