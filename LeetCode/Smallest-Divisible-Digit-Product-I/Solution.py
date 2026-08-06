3f256658284042fd4db8b06b250abb508d78c0df
Ashwin Srivastav <bashusrivastavmrj@gmail.com>
2026-08-06 23:09:39 +0530

    Refactor Solution class from C++ to Python

--- LeetCode/Smallest-Divisible-Digit-Product-I/Solution.py
+++ LeetCode/Smallest-Divisible-Digit-Product-I/Solution.py
@@ -1,12 +1,12 @@
-class Solution {
-public:
-    int smallestNumber(int n, int t) {
-        for (int i = n; i <= n + 9; ++i) {
-            int product = 1; 
-            for (int v = i; v > 0; v /= 10)
-                product *= v % 10;
-            if (product % t == 0) return i;
-        }
-        return -1; // unreachable.
-    }
-};
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        if n>9:
+            last=n%10;first=(n//10)%10;count=-1
+            for i in range(last,10):
+                count+=1
+                if (i*first)%t==0:
+                    return n+count
+            return int(str(first+1)+"0")
+        for i in range(n,n+10):
+            if i%t==0 or i==10:
+                return i
